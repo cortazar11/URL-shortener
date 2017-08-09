3e96eb6e10cc12954849b02d14d3f282b5e40955
System <none>
2017-08-09 19:20:15 +0000

    Checkpoint

--- datastore.js
+++ datastore.js
@@ -1,6 +1,15 @@
 var mongodb=require("mongodb")
+var MongoClient = mongodb.MongoClient;
+
 var MONGODB_URI ="mongodb://cortazar11:lasarte@ds141358.mlab.com:41358/freecodecamp"
 
-mongodb.connect(MONGODB_URI,function(err,db){
-  db.createCollection("mycollection")
+MongoClient.connect(MONGODB_URI,function(err,db){
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', MONGODB_URI);
+  }
+  
+  db.close()
+
 })
