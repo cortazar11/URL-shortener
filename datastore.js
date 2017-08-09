b51f983bc4c4a413bf872473029503144f3d01f0
System <none>
2017-08-09 19:09:15 +0000

    Checkpoint

--- datastore.js
+++ datastore.js
@@ -1,6 +1,6 @@
 var mongodb=require("mongodb")
-var MONGODB_URI ="mongodb://cortazar11:hernani1@ds141358.mlab.com:41358/freecodecamp"
+var MONGODB_URI ="mongodb://cortazar11:lasarte@ds141358.mlab.com:41358/freecodecamp"
 
 mongodb.connect(MONGODB_URI,function(err,db){
-  
+  db.createCollection("mycollection")
 })
