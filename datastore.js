275a36091c85d1465be431824b9f800779880e51
System <none>
2017-08-10 08:05:16 +0000

    Checkpoint

--- datastore.js
+++ datastore.js
@@ -3,6 +3,12 @@ var MongoClient = mongodb.MongoClient;
 
 var MONGODB_URI ="mongodb://cortazar11:lasarte@ds141358.mlab.com:41358/freecodecamp"
 
+var doc={
+            "firstName":"Pepe",
+            "lastName":"Gonzalez"
+            
+        }
+
 MongoClient.connect(MONGODB_URI,function(err,db){
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
@@ -10,7 +16,12 @@ MongoClient.connect(MONGODB_URI,function(err,db){
     console.log('Connection established to', MONGODB_URI);
   }
   
-  var collection=db.collection()
+  var collection=db.collection("url")
+  collection.insert(doc,function(){
+        if(err) throw err
+        console.log(JSON.stringify(doc))
+        db.close()
+    })
 
 
 })
